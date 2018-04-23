31a4decc4fb4853e1734ed7e4eaef0959d16073a
vashand <nickshannon83@gmail.com>
2018-04-23 14:34:52 -0600

    Add files via upload

--- HighScores.java
+++ HighScores.java
@@ -1,21 +1,38 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 
-	
 public class HighScores
 {
 	
+	String playerName;
+	int score;
 	
-	public void highscore() 
-			  throws IOException {
-			    String str = "Hello";
-			   
-			    BufferedWriter writer = new BufferedWriter(new FileWriter("scores.txt"));
-			    writer.write(str);
-			     
-			    writer.close();
-			}
-
+	
+	@Override
+	public String toString( )
+	{
+		System.out.println ( "HighScores [playerName=" + playerName + ", score=" + score + "]" );
+		return "HighScores [playerName=" + playerName + ", score=" + score + "]";
+	}
+	public HighScores(String playerName, int score)
+	{
+		super ( );
+		this.playerName = playerName;
+		this.score = score;
+	}
+	public String getPlayerName( )
+	{
+		return playerName;
+	}
+	public void setPlayerName( String playerName )
+	{
+		this.playerName = playerName;
+	}
+	public int getScore( )
+	{
+		return score;
+	}
+	public void setScore( int score )
+	{
+		this.score = score;
+	}
 	
 }
